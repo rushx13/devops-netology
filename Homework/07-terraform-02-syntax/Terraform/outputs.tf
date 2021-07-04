dd11dd933ea3fc2cacf5d64c1e0f33dd8358b596
Aleksey Averin <77511453+rushx13@users.noreply.github.com>
2021-07-04 20:05:08 +0300

    Create outputs.tf

---
+++ Homework/07-terraform-02-syntax/Terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "region" {
+  value = data.aws_region.current.name
+}
